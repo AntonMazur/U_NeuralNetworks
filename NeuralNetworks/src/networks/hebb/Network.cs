db4cc5ad3f9e0fbf285fbabfa82e95516c26f80f
Антон Лесников <toni.lesnikov.98@mail.ru>
2018-11-26 22:00:42 +0300

    Added core of Hebb network

--- NeuralNetworks/src/networks/hebb/Network.cs
+++ NeuralNetworks/src/networks/hebb/Network.cs
@@ -10,7 +10,63 @@ namespace NeuralNetworks.src.networks.hebb
     {
         public static Sample run(Sample[] samples, Sample testSample)
         {
+            
+            
+
             throw new NotImplementedException("No implementation for hebb network");
+        }
+
+        private static double[,] learn(Sample[] samples, HebbSample[] hSamples)
+        {
+            int sampleLen = samples[0].area;
+            HebbSample.resetState();
+            HebbSample.digitCount = (int)Math.Ceiling(Math.Log(samples.Length, 2));
+            double[,] weights = new double[HebbSample.digitCount, sampleLen];  // row - weights of edges from inputs to first neuron, column - weights of edges from the input to all neurons
+
+
+            for (int k = 0; k < samples.Length; k++)
+            {
+                var sampleArr = samples[k].asBitArray();
+                bool failOccured;
+
+                do
+                {
+                    failOccured = false;
+
+                    for (int i = 0; i < HebbSample.digitCount; i++)
+                    {
+                        var output = 0d;
+
+                        for (int j = 0; j < sampleLen; j++)
+                        {
+                            output += weights[i, j] * Utils.boolToNum(sampleArr[j]);
+                        }
+
+                        output = output > 0 ? 1 : -1;
+
+                        if (output * hSamples[i].getNeuronOutput(i) < 0)
+                        {
+                            failOccured = true;
+
+                            for (int j = 0; j < sampleLen; j++)
+                            {
+                                weights[i, j] = getWeightDelta(Utils.boolToNum(sampleArr[j]), output);
+                            }
+                        }
+
+                    }
+                } while (failOccured);
+
+            }
+
+            return weights;
+        }
+
+        private static double getWeightDelta(double input, double output)
+        {
+            if (input == 0) return 0;
+
+            return input * output > 0 ? 1 : -1;
         }
     }
 
@@ -21,10 +77,26 @@ namespace NeuralNetworks.src.networks.hebb
         private Sample sample;
         private double[] networkOutput;
 
-        HebbSample(Sample sample)
+        public HebbSample(Sample sample)
         {
             this.sample = sample;
             this.networkOutput = getNextSampleOutput();
+        }
+
+        public double getNeuronOutput(int neuronNum)
+        {
+            return Utils.boolToNum(((counter >> neuronNum) & 1) == 1);
+        }
+
+        public static void resetState()
+        {
+            digitCount = 0;
+            counter = 0;
+        }
+
+        public static HebbSample[] convertAll(Sample[] samples)
+        {
+            return samples.Select(s => new HebbSample(s)).ToArray();
         }
 
         private static double[] getNextSampleOutput()
@@ -38,5 +110,6 @@ namespace NeuralNetworks.src.networks.hebb
 
             return result;
         }
+
     }
 }
